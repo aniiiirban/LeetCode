9540041369e913fea74cf060085ff71150408a92
aniiiirban <92040986+aniiiirban@users.noreply.github.com>
2023-12-30 00:25:31 +0530

    Time: 650 ms (5.00%) | Memory: 44.8 MB (17.14%) - LeetSync

--- 2694-find-the-maximum-divisibility-score/find-the-maximum-divisibility-score.java
+++ 2694-find-the-maximum-divisibility-score/find-the-maximum-divisibility-score.java
@@ -1,43 +1,45 @@
-// class Solution {
-//     public int maxDivScore(int[] nums, int[] divisors) {
-// //         int max=0; int c=0; int res=0;
-// //         for(int i=0; i<divisors.length; i++)
-// //         {   max=Math.max(c, max);
-// //             c=0;
-// //             for(int j=0; j< nums.length; j++)
-// //             {
-// //                 if(nums[j]%divisors[i]==0)
-// //                 ++c;
-// //                 if(c>max)
-// //                 res=divisors[i];
-// //             }
-// //         } return res;
-// //     }
-// // }
-
 class Solution {
     public int maxDivScore(int[] nums, int[] divisors) {
-        
-       
-        int a=0;
-        int aa=Integer.MAX_VALUE;
-        for(int i=divisors.length-1;i>=0;i--){
-            int d=divisors[i];
-             int c=0;
-            for(int j=0;j<nums.length;j++){
-                if(nums[j]%d==0)
-                c++;
+        int max=0; int c=0; int res=divisors[0];
+        for(int i=0; i<divisors.length; i++)
+        {   max=Math.max(c, max);
+            c=0;
+            for(int j=0; j< nums.length; j++)
+            {
+                if(nums[j]%divisors[i]==0)
+                ++c;
+                if(c>max)
+                res=divisors[i];
+                if(c==max)
+                res=Math.min(res, divisors[i]);
             }
-            if(c==a){
-                aa=Math.min(aa,d);
-
-            }
-            if(c>a){
-                a=c;
-                aa=d;
-
-            }
-        }
-        return aa;
+        } return res;
     }
 }
+
+// class Solution {
+//     public int maxDivScore(int[] nums, int[] divisors) {
+        
+       
+//         int a=0;
+//         int aa=Integer.MAX_VALUE;
+//         for(int i=divisors.length-1;i>=0;i--){
+//             int d=divisors[i];
+//              int c=0;
+//             for(int j=0;j<nums.length;j++){
+//                 if(nums[j]%d==0)
+//                 c++;
+//             }
+//             if(c==a){
+//                 aa=Math.min(aa,d);
+
+//             }
+//             if(c>a){
+//                 a=c;
+//                 aa=d;
+
+//             }
+//         }
+//         return aa;
+//     }
+// }
